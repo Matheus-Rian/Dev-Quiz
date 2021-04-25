310dd00e2bb79306f5888c55f71d591ce3d0ea29
Matheus Rian <souzamatheusrian@gmail.com>
2021-04-25 12:21:28 -0300

    feat: get user ✨

--- lib/home/widgets/app_bar/app_bar_widget.dart
+++ lib/home/widgets/app_bar/app_bar_widget.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_dev_quiz/shared/models/user_model.dart';
+
 import '../score_card/score_card_widget.dart';
 
 import '../../../core/app_text_styles.dart';
@@ -5,7 +7,8 @@ import '../../../core/app_gradients.dart';
 import 'package:flutter/material.dart';
 
 class AppBarWidget extends PreferredSize {
-  AppBarWidget()
+  final UserModel user;
+  AppBarWidget({required this.user})
       : super(
             preferredSize: Size.fromHeight(250),
             child: Container(
@@ -25,7 +28,7 @@ class AppBarWidget extends PreferredSize {
                               style: AppTextStyles.title,
                               children: [
                                 TextSpan(
-                                    text: "Matheus Rian",
+                                    text: user.name,
                                     style: AppTextStyles.titleBold)
                               ])),
                           Container(
@@ -34,8 +37,7 @@ class AppBarWidget extends PreferredSize {
                             decoration: BoxDecoration(
                                 borderRadius: BorderRadius.circular(10),
                                 image: DecorationImage(
-                                  image: NetworkImage(
-                                      "https://avatars.githubusercontent.com/u/53922139?v=4"),
+                                  image: NetworkImage(user.photoUrl),
                                 )),
                           )
                         ],
