26e26a259a1647a8d8b431f4c9602c2618822a15
Matheus Rian <souzamatheusrian@gmail.com>
2021-04-25 23:30:46 -0300

    feat: quiz widget ✨

--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -1,37 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dev_quiz/challenge/widgets/answer/answer_widget.dart';
 import 'package:flutter_dev_quiz/core/app_text_styles.dart';
+import 'package:flutter_dev_quiz/shared/models/answer.model.dart';
+import 'package:flutter_dev_quiz/shared/models/question_model.dart';
 
-class QuizWidget extends StatelessWidget {
-  final String title;
-  const QuizWidget({Key? key, required this.title}) : super(key: key);
+class QuizWidget extends StatefulWidget {
+  final QuestionModel question;
+  final VoidCallback onChange;
+  const QuizWidget({Key? key, required this.question, required this.onChange})
+      : super(key: key);
 
+  @override
+  _QuizWidgetState createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
+  int indexSelected = -1;
+
+  AnswerModel answer(int index) => widget.question.answers[index];
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: [
+          SizedBox(
+            height: 64,
+          ),
           Text(
-            title,
+            widget.question.title,
             style: AppTextStyles.heading,
           ),
           SizedBox(
             height: 24,
           ),
-          AnswerQuestionWidget(
-              isRight: false,
-              isSelected: true,
-              title:
-                  "Possibilita a criação de aplicativos compilados nativamente"),
-          AnswerQuestionWidget(
-              title:
-                  "Possibilita a criação de aplicativos compilados nativamente"),
-          AnswerQuestionWidget(
-              title:
-                  "Possibilita a criação de aplicativos compilados nativamente"),
-          AnswerQuestionWidget(
-              title:
-                  "Possibilita a criação de aplicativos compilados nativamente"),
+          for (var i = 0; i < widget.question.answers.length; i++)
+            AnswerQuestionWidget(
+              answer: answer(i),
+              disabled: indexSelected != -1,
+              isSelected: indexSelected == i,
+              onTap: () {
+                indexSelected = i;
+                widget.onChange();
+                setState(() {});
+              },
+            )
         ],
       ),
     );
