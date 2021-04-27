d741cb9cc6b93711bd190d576b9dc04c8bd29ce0
Matheus Rian <souzamatheusrian@gmail.com>
2021-04-27 16:43:30 -0300

    feat: quiz widget ✨

--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -6,8 +6,8 @@ import 'package:flutter_dev_quiz/shared/models/question_model.dart';
 
 class QuizWidget extends StatefulWidget {
   final QuestionModel question;
-  final VoidCallback onChange;
-  const QuizWidget({Key? key, required this.question, required this.onChange})
+  final ValueChanged<bool> onSelected;
+  const QuizWidget({Key? key, required this.question, required this.onSelected})
       : super(key: key);
 
   @override
@@ -38,10 +38,11 @@ class _QuizWidgetState extends State<QuizWidget> {
               answer: answer(i),
               disabled: indexSelected != -1,
               isSelected: indexSelected == i,
-              onTap: () {
+              onTap: (value) {
                 indexSelected = i;
-                widget.onChange();
                 setState(() {});
+                Future.delayed(Duration(seconds: 1))
+                    .then((_) => widget.onSelected(value));
               },
             )
         ],
